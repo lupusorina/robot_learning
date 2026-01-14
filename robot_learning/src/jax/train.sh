415d7d13e70164434d35e15f4086dbbf5af2e4bf
Sorina <lupusorina@yahoo.com>
2026-01-14 08:00:50 -0800

    remove randomize from run

--- robot_learning/src/jax/train.sh
+++ robot_learning/src/jax/train.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
 
+# Memory management for CUDA/JAX
+# Reduce GPU memory usage to prevent OOM errors
+export XLA_CLIENT_MEM_FRACTION=0.7
+
+# Alternative: Disable command buffers (uncomment if still getting OOM)
+# export XLA_FLAGS='--xla_gpu_enable_command_buffer='
 
 #MUJOCO_GL=egl python3 train.py
 #echo "Script 1. MUJOCO_GL=egl python3 train.py Done."
 
-MUJOCO_GL=egl python3 train.py --randomize_body_ipos --randomize_torso_mass --randomize_link_masses
+MUJOCO_GL=egl python3 train_with_motion.py
