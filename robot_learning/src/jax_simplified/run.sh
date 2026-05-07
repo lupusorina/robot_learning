0ea4ec05974a7862afb97b6b179ec0b7169373e4
Sorina <lupusorina@yahoo.com>
2026-05-07 13:50:14 -0700

    add run script

--- robot_learning/src/jax_simplified/run.sh
+++ robot_learning/src/jax_simplified/run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# Match ``robot_learning/src/jax/train.py`` Brax batching (batch_size=256, num_minibatches=32).
-# SKRL ``rollouts`` = unroll_length * (256*32 // num_envs); with 8192 envs and unroll 20 -> rollouts 20.
-
-export MUJOCO_GL=egl; python skrl_ppo_pytorch.py  --train biped
+cd robot_learning/src/jax_simplified
+MUJOCO_GL=egl \
+python skrl_ppo_pytorch.py \
+  --train biped \
+  --wandb \
+  --video \
+  --run-name run_name
